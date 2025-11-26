7aed483d3255a7ae9367015f459044e61f834007
zuya <kazu.prgrm555y@gmail.com>
2025-11-26 23:03:37 +0900

    add: private function

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,28 +8,34 @@ export default class ApiClient {
   constructor(prefixUrl: string, headers: Record<string, string> = {}) {
     this.prefixUrl = prefixUrl;
     this.headers = headers;
-    this.api = ky.create({
-      prefixUrl,
-      headers: {
-        Accept: "application/json",
-        ...headers,
-      },
-    });
+    this.api = this.kyCreate();
   }
 
-  async get<T>(url: string | null = null): Promise<T> {
-    return this.api.get<T>(url ?? "").json();
+  async get<T>(url: string): Promise<T> {
+    return this.api.get<T>(url).json();
   }
 
+  // TODO: dataの型を定義
   async post<T>(url: string, data: unknown): Promise<T> {
     return this.api.post<T>(url, { json: data }).json();
   }
 
+  // TODO: dataの型を定義
   async put<T>(url: string, data: unknown): Promise<T> {
     return this.api.put<T>(url, { json: data }).json();
   }
 
   async delete<T>(url: string): Promise<T> {
     return this.api.delete<T>(url).json();
+  }
+
+  private kyCreate() {
+    return ky.create({
+      prefixUrl: this.prefixUrl,
+      headers: {
+        Accept: "application/json",
+        ...this.headers,
+      },
+    });
   }
 }
