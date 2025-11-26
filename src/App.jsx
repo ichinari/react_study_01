d845122e95dbbc7cf513f34f0393b48ccb316227
zuya <kazu.prgrm555y@gmail.com>
2025-11-26 22:08:38 +0900

    add: fetch and log Pokemon data on component mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
+import { PokemonApi } from "./api/Pokemon";
+
+const pokemonApi = new PokemonApi();
+
 function App() {
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const pokemonData = await pokemonApi.getPokemonList();
+      console.log(pokemonData);
+    };
+    fetchPokemonData();
+  }, []);
 
   return (
     <>
       <div>react project</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
