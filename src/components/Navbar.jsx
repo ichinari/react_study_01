d7d762413d6e35f9761e9834c6eb1c3f9d7b533b
zuya <kazu.prgrm555y@gmail.com>
2025-11-29 23:20:31 +0900

    refactor: simplify Navbar component structure by removing unnecessary fragments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,11 @@
 function Navbar() {
   return (
-    <>
-      <nav className="bg-blue-500 p-4 flex justify-center items-center">
-        <div className="container text-center">
-          <p className="text-white text-2xl font-bold">Pokemon Dictionary</p>
-        </div>
-      </nav>
-    </>
+    <div className="bg-blue-500 p-4 flex justify-center items-center">
+      <div className="container text-center">
+        <p className="text-white text-2xl font-bold">Pokemon Dictionary</p>
+      </div>
+    </div>
   );
-  )
 }
 
-export default Navbar
+export default Navbar;
