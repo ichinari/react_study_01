fcd445b2c96757927aa4ade9997d8b3a890fd77f
zuya <kazu.prgrm555y@gmail.com>
2025-11-29 23:13:45 +0900

    add: create Navbar component for the Pokemon Dictionary application

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-blue-500 p-4 flex justify-center items-center">
+        <div className="container text-center">
+          <p className="text-white text-2xl font-bold">Pokemon Dictionary</p>
+        </div>
+      </nav>
+    </>
+  );
+  )
+}
+
+export default Navbar
